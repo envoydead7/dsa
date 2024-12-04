d2dd28a2a98a8b161bbdb3d11b866190b27f3149
envoydead7 <mustafaabbas0912@gmail.com>
2024-12-04 16:00:22 +0530

    Adding basic programs

---
+++ c++ basics/datatypes.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  long long num;
+  char a;
+  float val;
+  double value;
+  cin>>n>>num>>a>>val>>value;
+  cout<<n<<endl<<num<<endl<<a<<endl<<val<<endl<<value<<endl;
+}
---
+++ c++ basics/sheet1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//program to print a string taking input from the user
+int main() {
+  string s;
+  cout<<"Enter the name: ";
+  cin>>s;
+  cout<<"Hello, "<<s<<endl;
+}
