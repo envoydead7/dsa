9a6e422df88188e3ce08664050a4ce917ec3ef1d
envoydead7 <mustafaabbas0912@gmail.com>
2024-12-21 00:39:43 +0530

    update evenNum

--- sheet2(loops)/evenNum.cpp
+++ sheet2(loops)/evenNum.cpp
@@ -1,13 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
     long long n;
-    cin>>n;
+    cin >> n;
 
-    for (int i = 1;i<=n;i++) {
-        if(i%2==0){
-          cout<<i<<endl;
+    bool foundEven = false; // Declare and initialize the boolean variable
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            cout << i << endl;
+            foundEven = true; // Set to true if an even number is found
         }
     }
+
+    if (!foundEven) { // Check if no even numbers were found after the loop
+        cout << -1 << endl;
+    }
+
+    return 0;
 }
